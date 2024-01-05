63557e52460f114f8bc0ac656adf0869dc7249a7
aaronberkson <155697754+aaronberkson@users.noreply.github.com>
2024-01-04 22:38:06 -0500

    Update index.ts

--- 3D_text/index.ts
+++ 3D_text/index.ts
@@ -1,7 +1,3 @@
-import { startBasicScene } from './basic_scene';
-import { startDingbatsScene } from './dingbats_scene';
 import { startStarWarsScene } from './star_wars_scene'
 
-// startBasicScene();
 startStarWarsScene();
-// startDingbatsScene();
